41b91923136881d894c0ef97f03fd44249096e7d
lvcoc <742818396@qq.com>
2020-01-18 12:32:20 +0800

    update

---
+++ Assets/Scripts/Helper/Renamer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEngine;
+
+public class Renamer : MonoBehaviour
+{
+    [ContextMenu("Rename")]
+    private void Rename()
+    {
+        for (int i = 0; i < this.parent.childCount; i++)
+        {
+            if (this.parent.GetChild(i).name.Contains(this.fromName) || this.toAppend)
+            {
+                if (this.toAppend)
+                {
+                    this.parent.GetChild(i).name = this.toName + this.parent.GetChild(i).name;
+                }
+                else
+                {
+                    this.parent.GetChild(i).name = this.parent.GetChild(i).name.Replace(this.fromName, this.toName);
+                }
+                if (this.parent.GetChild(i).childCount > 0)
+                {
+                    this.CheckChilds(this.parent.GetChild(i));
+                }
+            }
+        }
+    }
+
+    private void CheckChilds(Transform t)
+    {
+        for (int i = 0; i < t.childCount; i++)
+        {
+            if (t.GetChild(i).name.Contains(this.fromName) || this.toAppend)
+            {
+                if (this.toAppend)
+                {
+                    t.GetChild(i).name = this.toName + t.GetChild(i).name;
+                }
+                else
+                {
+                    t.GetChild(i).name = t.GetChild(i).name.Replace(this.fromName, this.toName);
+                }
+                this.CheckChilds(t.GetChild(i));
+            }
+        }
+    }
+
+    public bool toAppend;
+
+    public string fromName;
+
+    public string toName;
+
+    public Transform parent;
+}
